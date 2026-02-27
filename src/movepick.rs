6a5e50ed4a3bd872f0a8def4eb188c6aefaed8bb
Michael Whiteley <protonspring@gmail.com>
2026-02-27 12:14:55 -0700

    4x.
    bench 3437662

--- src/movepick.rs
+++ src/movepick.rs
@@ -226,13 +226,13 @@ impl MovePicker {
                 let pt = td.board.piece_on(mv.from()).piece_type();
 
                 if pt == PieceType::Queen {
-                    entry.score += 20000;
+                    entry.score += 40000;
                 }
                 else if pt == PieceType::Rook {
-                    entry.score += 10000;
+                    entry.score += 15000;
                 }
                 else if pt != PieceType::Pawn {
-                    entry.score += 4000;
+                    entry.score += 8000;
                 }
             }
 
