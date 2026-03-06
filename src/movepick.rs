6c7e24e97a732a48bbee7aa3e7783cfdec330b2f
Michael Whiteley <protonspring@gmail.com>
2026-03-06 09:15:07 -0700

    Simplify some threat scoring.
    bench 3393210

--- src/movepick.rs
+++ src/movepick.rs
@@ -216,11 +216,7 @@ impl MovePicker {
 
             // bonus for escaping capture
             if threatened.contains(mv.from()) {
-                if pt == PieceType::Queen {
-                    entry.score += 20000;
-                } else if pt != PieceType::Pawn {
-                    entry.score += 8000;
-                }
+                entry.score += 6000 * 3000 * pt as i32;
             }
 
             // Bonus for checking moves
