959f4025ce32094eea01401e4ffb05e68c5bac4f
Michael Whiteley <protonspring@gmail.com>
2026-03-17 12:29:41 -0600

    simplify capture scoring.
    bench 2795637

--- src/movepick.rs
+++ src/movepick.rs
@@ -156,24 +156,20 @@ impl MovePicker {
 
     fn score_noisy(&mut self, td: &ThreadData) {
         let threats = td.board.all_threats();
+        let in_check = !td.board.checkers().is_empty();
 
-        if td.board.checkers().is_empty() {
-            for entry in self.list.iter_mut() {
-                let mv = entry.mv;
-                let captured =
-                    if entry.mv.is_en_passant() { PieceType::Pawn } else { td.board.piece_on(mv.to()).piece_type() };
+        for entry in self.list.iter_mut() {
+            let mv = entry.mv;
+            let pt = td.board.piece_on(mv.from()).piece_type();
+            let captured =
+                if entry.mv.is_en_passant() { PieceType::Pawn } else { td.board.piece_on(mv.to()).piece_type() };
 
-                entry.score =
-                    16 * captured.value() + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured);
-            }
-        } else {
-            //in check
-            for entry in self.list.iter_mut() {
-                let mv = entry.mv;
-                let pt = td.board.piece_on(mv.from()).piece_type();
-
-                entry.score = 10000 - 1000 * pt as i32;
-            }
+            entry.score =
+                if in_check { 10000 - 1000 * pt as i32 }
+                else {
+                    td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
+                    + 16 * captured.value()
+                }
         }
     }
 
