51b4e26f2131c3edf341846ba7e867927d5b428c
Michael Whiteley <protonspring@gmail.com>
2026-04-01 16:55:52 -0600

    8k
    bench 3290107

--- src/movepick.rs
+++ src/movepick.rs
@@ -233,7 +233,7 @@ impl MovePicker {
                 + 6000 * offense[pt].contains(mv.to()) as i32;
 
             if pt == PieceType::Rook && king_ring_ortho.contains(mv.to()) {
-                entry.score += 3000;
+                entry.score += 8000;
             }
         }
     }
