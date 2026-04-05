54c282a84580aedbec935d363c7952e80ee45904
Michael Whiteley <protonspring@gmail.com>
2026-04-05 12:50:20 -0600

    use fullmove.
    bench 2899006

--- src/movepick.rs
+++ src/movepick.rs
@@ -236,7 +236,7 @@ impl MovePicker {
                 + td.conthist(ply, 4, mv)
                 + td.conthist(ply, 6, mv)
                 + escape[pt] * threatened[pt].contains(mv.from()) as i32
-                + td.board.scale_down_by_ply(12000) * td.board.checking_squares(pt).contains(mv.to()) as i32
+                + td.board.scale_up_by_ply(12000) * td.board.checking_squares(pt).contains(mv.to()) as i32
                 - 8000 * threatened[pt].contains(mv.to()) as i32
                 + 6000 * offense[pt].contains(mv.to()) as i32
                 + 5000 * (pt == PieceType::Rook && king_ring_ortho.contains(mv.to())) as i32;
