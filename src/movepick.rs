755fa80d77e01033d3e3f2d24525f99f4e9f1f75
Michael Whiteley <protonspring@gmail.com>
2026-04-05 13:13:05 -0600

    simplify mp scoring.
    bench 2539340

--- src/movepick.rs
+++ src/movepick.rs
@@ -228,9 +228,8 @@ impl MovePicker {
                 + td.conthist(ply, 4, mv)
                 + td.conthist(ply, 6, mv)
                 + escape[pt] * threatened[pt].contains(mv.from()) as i32
-                + 10000 * td.board.checking_squares(pt).contains(mv.to()) as i32
+                + 9000 * (offense[pt] | td.board.checking_squares(pt)).contains(mv.to()) as i32
                 - 8000 * threatened[pt].contains(mv.to()) as i32
-                + 6000 * offense[pt].contains(mv.to()) as i32
                 + 5000 * (pt == PieceType::Rook && king_ring_ortho.contains(mv.to())) as i32;
         }
     }
