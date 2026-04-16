a2e2aadc0989ba67640138917b4f99f57649715b
Michael Whiteley <protonspring@gmail.com>
2026-04-16 07:29:12 -0600

    Consolidate some scoring.
    bench 3066542

--- src/movepick.rs
+++ src/movepick.rs
@@ -155,26 +155,21 @@ impl MovePicker {
     fn score_noisy(&mut self, td: &ThreadData) {
         let threats = td.board.all_threats();
 
-        if td.board.in_check() {
-            for entry in self.list.iter_mut() {
-                let mv = entry.mv;
-                let pt = td.board.piece_on(mv.from()).piece_type();
+        for entry in self.list.iter_mut() {
+            let mv = entry.mv;
+            let pt = td.board.piece_on(mv.from()).piece_type();
+            let captured = if entry.mv.is_en_passant() {
+                PieceType::Pawn
+            } else {
+                td.board.piece_on(mv.to()).piece_type()
+            };
 
-                entry.score = 10000 - 1000 * pt as i32;
-            }
-        } else {
-            for entry in self.list.iter_mut() {
-                let mv = entry.mv;
-                let captured =
-                    if entry.mv.is_en_passant() { PieceType::Pawn } else { td.board.piece_on(mv.to()).piece_type() };
+            entry.score = 10000 - 1000 * pt as i32
+                + !td.board.in_check() as i32 *
 
-                entry.score =
-                    16 * captured.value() + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured);
-
-                if mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen {
-                    entry.score += 4000;
-                }
-            }
+                    (16 * captured.value()
+                    + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
+                    + 4000 * (mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen) as i32);
         }
     }
 
