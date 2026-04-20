a6d0cfbcfc2abe4a3c4f1b44a8fb1b179f14b31b
Michael Whiteley <protonspring@gmail.com>
2026-04-20 13:59:56 -0600

    -4 * pt value.
    bench 3192947

--- src/movepick.rs
+++ src/movepick.rs
@@ -162,6 +162,7 @@ impl MovePicker {
             let pt = td.board.type_on(mv.from());
 
             entry.score = 16 * captured.value()
+                - 4 * pt.value()
                 + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
                 + 4000 * (mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen) as i32
                 + (200000 - 20000 * pt as i32) * td.board.in_check() as i32;
