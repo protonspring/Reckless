dfba6d158d8dff399b7a060e36765a60f8aab2c7
Michael Whiteley <protonspring@gmail.com>
2026-04-23 21:32:37 -0600

    -2k for moving into a any threat.
    bench 3287549

--- src/movepick.rs
+++ src/movepick.rs
@@ -223,7 +223,8 @@ impl MovePicker {
                 + 9325 * td.board.checking_squares(pt).contains(mv.to()) as i32
                 - 7584 * threatened[pt].contains(mv.to()) as i32
                 + 5000 * offense[pt].contains(mv.to()) as i32
-                - 4000 * wall_pawns.contains(mv.from()) as i32;
+                - 4000 * wall_pawns.contains(mv.from()) as i32
+                - 2000 * threats.contains(mv.to()) as i32;
         }
     }
 }
