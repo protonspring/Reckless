05cf7f220e84860e276bcecb0532fbdd0252e237
Michael Whiteley <protonspring@gmail.com>
2026-04-23 21:21:29 -0600

    1k mobility bonus per square.
    bench 3101823

--- src/movepick.rs
+++ src/movepick.rs
@@ -229,7 +229,7 @@ impl MovePicker {
 
             //mobility bonus
             let mobility = attacks(piece, mv.from(), td.board.occupancies()).popcount() as i32;
-            entry.score += 500 * mobility;
+            entry.score += 1000 * mobility;
 
         }
     }
