2c9bdf87f933346b197fcae62c4e36988d3bfe64
Michael Whiteley <protonspring@gmail.com>
2026-04-24 07:52:22 -0600

    8k for outposts.
    bench 2861261

--- src/movepick.rs
+++ src/movepick.rs
@@ -238,7 +238,7 @@ impl MovePicker {
                 //println!("{}", td.board);
                 //println!("Move: {}-{}", mv.from(), mv.to());
                 //println!("{}", passed_space);
-                entry.score += 4000;
+                entry.score += 8000;
             }
         }
     }
