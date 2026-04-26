5700d98350d8c69d0db18710e8d0d982ecd20218
Michael Whiteley <protonspring@gmail.com>
2026-04-26 17:46:08 -0600

    tmp

--- src/movepick.rs
+++ src/movepick.rs
@@ -37,22 +37,11 @@ impl MovePicker {
         }
     }
 
-    pub const fn new_probcut(threshold: i32) -> Self {
+    pub const fn new_nonmain(threshold: Option<i32>) -> Self {
         Self {
             list: MoveList::new(),
             tt_move: Move::NULL,
-            threshold: Some(threshold),
-            stage: Stage::GenerateNoisy,
-            bad_noisy: ArrayVec::new(),
-            bad_noisy_idx: 0,
-        }
-    }
-
-    pub const fn new_qsearch() -> Self {
-        Self {
-            list: MoveList::new(),
-            tt_move: Move::NULL,
-            threshold: None,
+            threshold: threshold,
             stage: Stage::GenerateNoisy,
             bad_noisy: ArrayVec::new(),
             bad_noisy_idx: 0,
