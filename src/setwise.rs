53ff0e27b8d05ea23535d245ad965ce1009e14ea
Michael Whiteley <protonspring@gmail.com>
2026-04-28 16:22:22 -0600

    Some cleanups in setwise attacks.
    bench 2569711

--- src/setwise.rs
+++ src/setwise.rs
@@ -1,4 +1,4 @@
-use crate::types::{Bitboard, Color, File, Rank};
+use crate::types::{Bitboard, Color, File, Rank, Square};
 
 const A: Bitboard = Bitboard::file(File::A);
 const B: Bitboard = Bitboard::file(File::B);
@@ -10,28 +10,23 @@ const R7: Bitboard = Bitboard::rank(Rank::R7);
 const R8: Bitboard = Bitboard::rank(Rank::R8);
 
 pub fn pawn_attacks_setwise(bb: Bitboard, color: Color) -> Bitboard {
-    let (up_right, up_left) = match color {
-        Color::White => (9, 7),
-        Color::Black => (-7, -9),
-    };
+    let up_right = Square::UP[color] + Square::RIGHT;
+    let up_left = Square::UP[color] + Square::LEFT;
 
-    let right_attacks = (bb & !Bitboard::file(File::H)).shift(up_right);
-    let left_attacks = (bb & !Bitboard::file(File::A)).shift(up_left);
-
-    right_attacks | left_attacks
+    (bb & !H).shift(up_right) | (bb & !A).shift(up_left)
 }
 
 #[cfg(not(target_feature = "avx2"))]
 #[inline]
 pub fn knight_attacks_setwise(bb: Bitboard) -> Bitboard {
-    (bb & !(A | B | R8)).shift(6)
-        | (bb & !(A | R7 | R8)).shift(15)
-        | (bb & !(H | R7 | R8)).shift(17)
-        | (bb & !(G | H | R8)).shift(10)
-        | (bb & !(G | H | R1)).shift(-6)
-        | (bb & !(H | R1 | R2)).shift(-15)
-        | (bb & !(A | R1 | R2)).shift(-17)
-        | (bb & !(A | B | R1)).shift(-10)
+    (bb & !(A | B)).shift(6)
+        | (bb & !A).shift(15)
+        | (bb & !H).shift(17)
+        | (bb & !(G | H)).shift(10)
+        | (bb & !(G | H)).shift(-6)
+        | (bb & !H).shift(-15)
+        | (bb & !A).shift(-17)
+        | (bb & !(A | B)).shift(-10)
 }
 
 #[cfg(target_feature = "avx2")]
