29ddf762ff68c4b1b61315645a84b6f14cff78ae
Michael Whiteley <protonspring@gmail.com>
2026-04-29 15:12:35 -0600

    Hard bound is 75% of main time only.

--- src/time.rs
+++ src/time.rs
@@ -37,7 +37,7 @@ impl TimeManager {
                 let hard_scale = 0.742;
 
                 let soft_bound = (soft_scale * main.saturating_sub(move_overhead) as f64 + 0.75 * inc as f64) as u64;
-                let hard_bound = (hard_scale * main.saturating_sub(move_overhead) as f64 + 0.75 * inc as f64) as u64;
+                let hard_bound = (hard_scale * main.saturating_sub(move_overhead) as f64) as u64;
 
                 soft = soft_bound.min(main.saturating_sub(move_overhead));
                 hard = hard_bound.min(main.saturating_sub(move_overhead));
