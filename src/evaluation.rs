e20f06a329f89c957f023d68b25b81195aa99ab6
Michael Whiteley <protonspring@gmail.com>
2026-05-01 20:02:40 -0600

    scale down later.
    bench 2774019

--- src/evaluation.rs
+++ src/evaluation.rs
@@ -5,7 +5,20 @@ pub fn correct_eval(td: &ThreadData, raw_eval: i32, correction_value: i32) -> i3
         + td.optimism[td.board.side_to_move()] * (1487 + td.board.material()))
         / 26685;
 
-    eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
+ 
+    //let old_scale = (200 - td.board.halfmove_clock() as i32) / 200;
+    let fmr_scale = (200.0 - ((td.board.halfmove_clock() as f32 - 35.0)/ 12.0).exp()) / 200.0;
+    //println!("FMR {}, {}", td.board.halfmove_clock(), fmr_scale);
+    //println!("fmr: {}, old {}, new: {}", td.board.halfmove_clock(), old_scale, fmr_scale);
+
+    //eval = (eval as f32 * fmr_scale) as i32;
+
+    //eval = eval * old_scale; //(200 - td.board.halfmove_clock() as i32) / 200;
+
+    //eval = eval * (200 - td.board.halfmove_clock() as i32) / 200;
+    eval = (eval as f32 * fmr_scale) as i32;
+    //println!("fmr: {}, old {}, new scale: {}, eval: {}, new eval: {}", td.board.halfmove_clock(), old_scale, fmr_scale, eval, eval2);
+    //println!("fmr: {}, new scale: {}, new eval: {}", td.board.halfmove_clock(), fmr_scale, eval);
 
     eval += correction_value;
 
