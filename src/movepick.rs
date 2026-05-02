207750c8a88c7e563cdb0f69c93f379fbf0f011e
Michael Whiteley <protonspring@gmail.com>
2026-05-02 13:19:08 -0600

    remove noisy history.
    bench 2762963

--- src/movepick.rs
+++ src/movepick.rs
@@ -149,7 +149,7 @@ impl MovePicker {
     }
 
     fn score_noisy(&mut self, td: &ThreadData) {
-        let threats = td.board.all_threats();
+        //let threats = td.board.all_threats();
 
         for entry in self.list.iter_mut() {
             let mv = entry.mv;
@@ -157,7 +157,7 @@ impl MovePicker {
             let pt = td.board.type_on(mv.from());
 
             entry.score = 16 * captured.value()
-                + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
+                //+ td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
                 + 4000 * (mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen) as i32
                 + (200000 - 20000 * pt as i32) * td.board.in_check() as i32;
         }
