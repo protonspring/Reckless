f59197c93fd07d8cd1b711ef6ac93a0858c1fa40
Michael Whiteley <protonspring@gmail.com>
2026-05-05 11:34:33 -0600

    scale checking bonus
    bench 2173106

--- src/movepick.rs
+++ src/movepick.rs
@@ -205,6 +205,8 @@ impl MovePicker {
             Bitboard(0)
         };
 
+        let checking_bonus = [12000, 9000, 9000, 5000, 3000, 0];
+
         for entry in self.list.iter_mut() {
             let mv = entry.mv;
             let pt = td.board.type_on(mv.from());
@@ -215,7 +217,7 @@ impl MovePicker {
                 + td.conthist(ply, 4, mv)
                 + td.conthist(ply, 6, mv)
                 + escape[pt] * threatened[pt].contains(mv.from()) as i32
-                + 9325 * td.board.checking_squares(pt).contains(mv.to()) as i32
+                + checking_bonus[pt] * td.board.checking_squares(pt).contains(mv.to()) as i32
                 - 7584 * threatened[pt].contains(mv.to()) as i32
                 + 5000 * offense[pt].contains(mv.to()) as i32
                 - 4000 * wall_pawns.contains(mv.from()) as i32;
