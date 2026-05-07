46f993cbd8dfcd6d9f5eea2595da3b1baee1ecb1
Michael Whiteley <protonspring@gmail.com>
2026-05-07 09:42:34 -0600

    2k
    bench 2218689

--- src/movepick.rs
+++ src/movepick.rs
@@ -166,7 +166,7 @@ impl MovePicker {
             entry.score = 16 * captured.value()
                 + td.noisy_history.get(threats, td.board.moved_piece(mv), mv.to(), captured)
                 + 4000 * (mv.is_promotion() && mv.promo_piece_type() == PieceType::Queen) as i32
-                + 4000 * dbl_threats.contains(mv.from()) as i32
+                + 2000 * dbl_threats.contains(mv.from()) as i32
                 + (200000 - 20000 * pt as i32) * td.board.in_check() as i32;
         }
     }
