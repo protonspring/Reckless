2e33635e4c9a35b2c901a48175b4daacd72d63cb
Michael Whiteley <protonspring@gmail.com>
2026-05-09 09:43:39 -0600

    knight attacks setwise.
    bench 2287208

--- src/setwise.rs
+++ src/setwise.rs
@@ -1,4 +1,5 @@
 use crate::types::{Bitboard, Color, File, Rank};
+use crate::lookup::{knight_attacks};
 
 const A: Bitboard = Bitboard::file(File::A);
 const B: Bitboard = Bitboard::file(File::B);
@@ -18,47 +19,14 @@ pub fn pawn_attacks_setwise(bb: Bitboard, color: Color) -> Bitboard {
     (bb & !H).shift(up_right) | (bb & !A).shift(up_left)
 }
 
-#[cfg(not(target_feature = "avx2"))]
-#[inline]
-pub fn knight_attacks_setwise(bb: Bitboard) -> Bitboard {
-    (bb & !(A | B | R8)).shift(6)
-        | (bb & !(A | R7 | R8)).shift(15)
-        | (bb & !(H | R7 | R8)).shift(17)
-        | (bb & !(G | H | R8)).shift(10)
-        | (bb & !(G | H | R1)).shift(-6)
-        | (bb & !(H | R1 | R2)).shift(-15)
-        | (bb & !(A | R1 | R2)).shift(-17)
-        | (bb & !(A | B | R1)).shift(-10)
-}
-
-#[cfg(target_feature = "avx2")]
-#[inline]
-pub fn knight_attacks_setwise(bb: Bitboard) -> Bitboard {
-    use core::arch::x86_64::*;
-
-    unsafe {
-        let mask_a = _mm256_set_epi64x(
-            !(A | B | R8).0 as i64,
-            !(A | R7 | R8).0 as i64,
-            !(H | R7 | R8).0 as i64,
-            !(G | H | R8).0 as i64,
-        );
-        let mask_b = _mm256_set_epi64x(
-            !(G | H | R1).0 as i64,
-            !(H | R1 | R2).0 as i64,
-            !(A | R1 | R2).0 as i64,
-            !(A | B | R1).0 as i64,
-        );
-
-        let bb = _mm256_set1_epi64x(bb.0 as i64);
-        let a = _mm256_and_si256(bb, mask_a);
-        let b = _mm256_and_si256(bb, mask_b);
-        let a = _mm256_sllv_epi64(a, _mm256_set_epi64x(6, 15, 17, 10));
-        let b = _mm256_srlv_epi64(b, _mm256_set_epi64x(6, 15, 17, 10));
-        fold_to_bitboard(_mm256_or_si256(a, b))
+pub fn knight_attacks_setwise(knights: Bitboard) -> Bitboard {
+    let mut attacks = Bitboard(0);
+    for knight in knights {
+        attacks |= knight_attacks(knight);
     }
+    attacks
 }
-
+     
 #[cfg(not(target_feature = "avx2"))]
 #[inline]
 pub fn bishop_attacks_setwise(bb: Bitboard, occupancies: Bitboard) -> Bitboard {
